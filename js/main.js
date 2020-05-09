933cd411e3e39bb8f49410abf62a1f6b7ff6eef1
Tim Scully <hello@timscully.co.uk>
2020-05-09 18:59:53 +0100

    added custom messages for response depending on percentage

--- js/main.js
+++ js/main.js
@@ -7,5 +7,17 @@ submitBtn.onclick = function(){
 function loveCalculator(weight, height) {
     var n = Math.random();
     var love = Math.floor(n * 100) + 1;
-    document.getElementById("result-text").innerHTML = "Your love compatibility is " + love + "%";
+    var ratingElement = "";
+
+    if (love < 30) {
+      ratingElement = "better luck next time"
+    } else if (love > 30 && love <= 70)  {
+      ratingElement = "there might be a chance"
+    } else if (love == 100)  {
+      ratingElement = "when's the wedding"
+    } else {
+      ratingElement = "congratuations, your in!"
+    }
+
+    document.getElementById("result-text").innerHTML = "Your love compatibility is " + love + "%<br><br>" + ratingElement;
 }
